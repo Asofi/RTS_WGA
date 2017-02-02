ce12cd026fa6cb3b75e2187188c7f30c361ba5d4
Oleg Yanushevsky <o.yanushevsky@gmail.com>
2017-02-02 17:34:12 +0300

    changing

---
+++ Assets/Splash.shader
@@ -0,0 +1,41 @@
+﻿Shader "Custom/Splash" {
+	Properties {
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Center("Center", Vector) = (0, 0, 0, 0) 
+		_Radius("Radius", Float) = 0.5
+		_Color("Color", Color) = (1, 0, 0, 1)
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+			float3 worldPos;
+		};
+
+		fixed4 _Color;
+		float3	_Center;
+		float _Radius;
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			float d = distance(_Center, IN.worldPos);
+			if (d < _Radius)
+				o.Albedo = _Color;
+			else
+				o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb;
+				
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ Assets/SplashController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashController : MonoBehaviour {
+
+    public Material SplashMat;
+    public float Radius;
+    public Vector3 Center;
+    public float TargetRadius;
+    public Texture MainTex;
+    public RenderTexture render;
+
+	void Start () {
+        MainTex = SplashMat.GetTexture("_MainTex");
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StopAllCoroutines();
+            Radius = 0;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                Center = hit.point;
+
+            SplashMat.SetVector("_Center", Center);
+            StartCoroutine(Splashing());
+        }
+        SplashMat.SetFloat("_Radius", Radius);
+    }
+
+    IEnumerator Splashing()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            float newSpeed = Mathf.Lerp(Radius, TargetRadius, t);
+            Radius = newSpeed;
+            t += 0.1f * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        SplashMat.SetTexture("_MainTex", MainTex);
+        Shit();
+    }
+
+    void Shit()
+    {
+        var mat = new Material(Shader.Find("Custom/Splash"));
+        Graphics.Blit(MainTex, render, mat);
+    }
+}
